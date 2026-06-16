9f065d282ffe8a8e06f51729683762f01f96fe87
abuki-dev <abubekra04@gmail.com>
2026-06-16 07:21:18 +0300

    sort file created

---
+++ Day12/Sort.js
@@ -0,0 +1,9 @@
+let rank = [6, 8, 9, 20, 5];
+for (let index = 0; index < rank.length; index++) {
+  const element = rank[index];
+
+}
+//start at the fisrt element
+//if the index is included in the sorteds continue
+//else if i get greter number insert its idex as array
+//
