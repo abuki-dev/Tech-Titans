1e2b22d7ba43c64dfe1b61278f283fb353cb2b63
abuki-dev <abubekra04@gmail.com>
2026-06-17 20:46:14 +0300

    prompt number accepting only

---
+++ Day15/aceptutilend.js
@@ -0,0 +1,66 @@
+//accepting form the user until canceled or entered invalid input
+//creatign function
+//asking the user anumber
+//if currunt is  number break or cannvele the operation and display sum
+
+// function Accept_until() {
+//   let sum = 0;
+//   do {
+//     let input = prompt("peas enter number", 0);
+//     if (input == "0" || input == null || typeof input == "string") break;
+//     input = Number(input);
+//     sum += input;
+//   } while (true);
+//   return sum;
+// }
+// let result = Accept_until();
+// alert(`the sum is ${result}`);
+// function readNumber() {
+//   while (true) {
+//     let num = prompt("enter the number:");
+
+//     if (num === null || num === "") {
+//       return null;
+//     }
+//     if (!isNaN(num)) {
+//       console.log(num);
+//       return Number(num);
+//     } else {
+//       console.log("incorrect");
+//     }
+//   }
+// }
+
+// readNumber();
+
+// function readNumber() {
+//   let number;
+//   do {
+//     number = prompt("Enter a number please?", 0);
+//     if (number === null || num === "") return null;
+//   } while (!isFinite(num));
+
+//   return +number;
+// }
+
+// alert(`Read: ${readNumber()}`);
+
+// function readNumber() {
+//     while (true) {
+//         let num = prompt('enter the number:');
+
+//         if (num === null || num === "") {
+//             return null;
+//         }
+
+//         if (!isNaN(num)) {
+//              alert(num);
+//             return Number(num);
+//         }
+//         else{
+//         alert('incorrect');
+//         }
+//     }
+// }
+
+//  readNumber();
