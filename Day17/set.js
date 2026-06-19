5d57b34947c307b11e0a106eafc3bc01d632490d
abuki-dev <abubekra04@gmail.com>
2026-06-19 11:41:34 +0300

    ste pracitses

---
+++ Day17/set.js
@@ -0,0 +1,11 @@
+//sets do not hav keys only values stores only singel values simpy are arrays
+let numbers = new Set();
+numbers.add(6);
+numbers.add(5);
+numbers.add(4);
+numbers.add(3);
+numbers.add(2);
+console.log(numbers);
+
+//numbers.delete(6);
+console.log(numbers.entries());
