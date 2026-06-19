e21072d70ae463b6031d7cd8a371eb4033bb7475
abuki-dev <abubekra04@gmail.com>
2026-06-19 10:27:47 +0300

    sbscription motly report

---
+++ Day16/subscriptions.js
@@ -0,0 +1,12 @@
+//intiitialize report object
+//for ecah user
+//add curunt userst price to total moth revenue
+//got to subscription plan counts
+//if currunt subrcriptuin type not exists in subcription plans create it as key
+//eles add +1 to this subrcription type
+//
+//collect every user to by suscription type
+//
+//
+//
+//if currunt user is incative or the status is canceled push user to inactives
