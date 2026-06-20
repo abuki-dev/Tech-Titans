c290ee1197c77f72f01cc300e9a8568462dc312f
abuki-dev <abubekra04@gmail.com>
2026-06-20 08:55:50 +0300

    task two practice

---
+++ Day17/task2.js
@@ -0,0 +1,14 @@
+function aclean(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let sorted = arr[i].toLowerCase().split("").sort().join("");
+    obj[sorted] = arr[i];
+  }
+
+  return Object.values(obj);
+}
+
+let arr = ["PAN", "teachers", "cheaters", "nap", "ear", "era", "hectares"];
+
+console.log(aclean(arr));
