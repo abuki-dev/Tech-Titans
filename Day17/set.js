465218316e1f0ab710841a74957896db1222e3f3
abuki-dev <abubekra04@gmail.com>
2026-06-20 08:56:05 +0300

    update set.js

--- Day17/set.js
+++ Day17/set.js
@@ -1,4 +1,4 @@
-//sets do not hav keys only values stores only singel values simpy are arrays
+// //sets do not hav keys only values stores only singel values simpy are arrays
 let numbers = new Set();
 numbers.add(6);
 numbers.add(5);
@@ -9,3 +9,8 @@ console.log(numbers);
 
 //numbers.delete(6);
 console.log(numbers.entries());
+
+//making teh fisrt string upercase then return the name
+//split tthe namke teh split[0] to upper case the join back
+//slice 1 then slice 1 to end to upper fist then concat
+
