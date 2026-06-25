43a93e6b6da98122af6db19bf376e7117f9487e6
abuki-dev <abubekra04@gmail.com>
2026-06-25 23:37:26 +0300

    taks don by our team

--- Day21/task.js
+++ Day21/task.js
@@ -1,8 +1,8 @@
 let user = { name: "John", years: 30 };
 let { name, years, isAdmin = false } = user;
-console.log(name);
-console.log(years);
-console.log(isAdmin);
+//console.log(name);
+//console.log(years);
+//console.log(isAdmin);
 
 let salaries = {
   John: 100,
@@ -22,4 +22,34 @@ function topSaley(saleofusre) {
   }
   return employe;
 }
-console.log(topSaley(array));
+//console.log(topSaley(array));
+const rectangle = {
+  width: 50,
+  height: 100,
+  color: "Blue",
+};
+function getArea({ height, width }) {
+  return height + width;
+}
+console.log("Area is :", getArea(rectangle));
+
+//
+let book = {
+  title: "the Great gatsby",
+  page: 218,
+};
+let { title: Booktitle, author = "unkown" } = book;
+console.log(Booktitle);
+console.log(author);
+
+let student = {
+  name: "John",
+  address: {
+    city: "London",
+    country: "UK",
+  },
+};
+let {
+  address: { city, country },
+} = student;
+console.log(city, country);
