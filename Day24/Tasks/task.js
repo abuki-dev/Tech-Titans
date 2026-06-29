fd328a02a0cde3d1128481b1f0ef4382c17e33b7
abuki-dev <abubekra04@gmail.com>
2026-06-29 17:23:08 +0300

    creating elements untile user cancelled

---
+++ Day24/Tasks/task.js
@@ -0,0 +1,20 @@
+//goals is creating list elemnts until user cnaceles it
+//do while loop
+//accept text what th user eneterd the creta li eleen tan dappent it with the rest
+//chen if true the prompt continue elese return
+if (document.getElementById("task1")) {
+  let container = document.getElementById("container");
+  let text;
+  callbackme();
+}
+function callbackme() {
+  if ((text = prompt("enter the text you wnat to add "))) {
+    let li = document.createElement("li");
+    li.textContent = text;
+    container.append(li);
+    alert(`an Html ${li.nodeName} element created with ${li.textContent}`);
+    setTimeout(() => {
+      callbackme();
+    }, 500);
+  }
+}
